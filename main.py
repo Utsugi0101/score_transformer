9ab89c8b441510933952cc75163f895cda007b0d
Utsugi0101 <soraki0101@gmail.com>
2024-11-18 00:34:34 +0900

    https接続の設定

--- main.py
+++ main.py
@@ -86,4 +86,5 @@ def set_secure_headers(response):
     return response
 
 if __name__ == '__main__':
-    app.run(debug=False, host='0.0.0.0', port=8000)
+    app.run(ssl_context=('cert.pem', 'key.pem'), host='0.0.0.0', port=8000)
+
