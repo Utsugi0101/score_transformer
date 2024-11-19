7342e374b167f6d2c4be0b8009b0928f8a2f2271
Utsugi0101 <soraki0101@gmail.com>
2024-11-19 22:02:42 +0900

    初期化の設定

--- App.py
+++ App.py
@@ -62,3 +62,7 @@ def upload_file():
 
     # PDFファイルを返す
     return send_file(output_path, as_attachment=True)
+if __name__ == '__main__':
+    
+    app.debug = True
+    app.run(host='localhost', port=8888)
